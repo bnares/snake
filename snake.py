bec6cfc3b8e1a5de1326184ce1fd1bec4a27bb42
Piotr Ostrouch <piotr.ostrouch@gmail.com>
2021-01-12 18:19:48 +0100

    creating surface

--- snake.py
+++ snake.py
@@ -1,8 +1,21 @@
 import pygame
+import sys
 
 pygame.init()
 
 
+clock = pygame.time.Clock()
+win = pygame.display.set_mode((400,500))
+win.fill((175,215,70))
+test_surface = pygame.Surface((100,200))
+test_surface.fill((255,0,0))
 
 
-
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    win.blit(test_surface, (50,100))
+    pygame.display.update()
+    clock.tick(60)

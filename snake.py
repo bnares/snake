52d27bb59e4070d995543e8b6b1b83579a361d8c
Piotr Ostrouch <piotr.ostrouch@gmail.com>
2021-01-13 14:40:05 +0100

    body of the snake

--- snake.py
+++ snake.py
@@ -13,10 +13,71 @@ class Snake:
         self.direction = Vector2(1,0)
         self.new_block = False
 
+        self.head_up = pygame.image.load(r"D:\calculation\snake\Snake-main\Snake-main\Graphics\head_up.png").convert_alpha()
+        self.head_down = pygame.image.load(r"D:\calculation\snake\Snake-main\Snake-main\Graphics\head_down.png").convert_alpha()
+        self.head_right = pygame.image.load(r"D:\calculation\snake\Snake-main\Snake-main\Graphics\head_right.png").convert_alpha()
+        self.head_left = pygame.image.load(r"D:\calculation\snake\Snake-main\Snake-main\Graphics\head_left.png").convert_alpha()
+
+        self.tail_up = pygame.image.load(r"D:\calculation\snake\Snake-main\Snake-main\Graphics\tail_up.png").convert_alpha()
+        self.tail_down = pygame.image.load(r"D:\calculation\snake\Snake-main\Snake-main\Graphics\tail_down.png").convert_alpha()
+        self.tail_right = pygame.image.load(r"D:\calculation\snake\Snake-main\Snake-main\Graphics\tail_right.png").convert_alpha()
+        self.tail_left = pygame.image.load(r"D:\calculation\snake\Snake-main\Snake-main\Graphics\tail_left.png").convert_alpha()
+
+        self.body_vertical = pygame.image.load(r"D:\calculation\snake\Snake-main\Snake-main\Graphics\body_vertical.png").convert_alpha()
+        self.body_horizontal = pygame.image.load(r"D:\calculation\snake\Snake-main\Snake-main\Graphics\body_horizontal.png").convert_alpha()
+
+        self.body_tr = pygame.image.load(r"D:\calculation\snake\Snake-main\Snake-main\Graphics\body_tr.png").convert_alpha()
+        self.body_tl = pygame.image.load(r"D:\calculation\snake\Snake-main\Snake-main\Graphics\body_tl.png").convert_alpha()
+        self.body_br = pygame.image.load(r"D:\calculation\snake\Snake-main\Snake-main\Graphics\body_br.png").convert_alpha()
+        self.body_bl = pygame.image.load(r"D:\calculation\snake\Snake-main\Snake-main\Graphics\body_bl.png").convert_alpha()
+        self.crunch_sound = pygame.mixer.Sound(r"D:\calculation\snake\Snake-main\Snake-main\Sound\crunch.wav")
+        self.head = self.head_right
+        self.tail = self.tail_left
+        self.snake_body = self.body_vertical
+
     def draw_snake(self):
-        for block in self.body:
+
+        self.update_head_graphics()
+        self.update_tail_graphics()
+        for index, block in enumerate(self.body):
             snake = pygame.Rect(block.x*cell_size, block.y*cell_size, cell_size, cell_size)
-            pygame.draw.rect(win, (183,111,122), snake)
+            if index ==0:
+                win.blit(self.head, snake)
+
+            elif index == len(self.body) - 1:
+                win.blit(self.tail,snake)
+
+            else:
+                previous_block = self.body[index+1] - block
+                next_block = self.body[index-1]-block
+                if previous_block.y == next_block.y:
+                    self.snake_body = self.body_vertical
+                    win.blit(self.snake_body, snake)
+                elif previous_block.x == next_block.x:
+                    self.snake_body = self.body_horizontal
+                    win.blit(self.snake_body, snake)
+                else:
+                    if ((previous_block.x == -1 and next_block.y ==-1) or (previous_block.y ==-1 and next_block.x ==-1)):
+                        win.blit(self.body_tl, snake)
+
+                    if ((previous_block.x == -1 and next_block.y ==-1) or (previous_block.y ==-1 and next_block.x ==-1)):
+                        win.blit(self.body_tl, snake)
+
+                    if ((previous_block.x == -1 and next_block.y ==-1) or (previous_block.y ==-1 and next_block.x ==-1)):
+                        win.blit(self.body_tl, snake)
+
+                    if ((previous_block.x == -1 and next_block.y ==-1) or (previous_block.y ==-1 and next_block.x ==-1)):
+                        win.blit(self.body_tl, snake)
+
+
+
+
+
+            #else:
+               # pygame.draw.rect(win, (183,111,122), snake)
+            #pygame.draw.rect(win, (183,111,122), snake)
+
+
 
     def move_snake(self):
         if self.new_block == False:
@@ -32,6 +93,30 @@ class Snake:
 
     def add_block(self):
         self.new_block = True
+
+
+    def update_head_graphics(self):
+        head_relation = self.body[1] - self.body[0]
+
+        if head_relation == Vector2(1,0):
+            self.head = self.head_left
+        elif head_relation == Vector2(-1,0):
+            self.head = self.head_right
+        elif head_relation == Vector2(0,1):
+            self.head = self.head_up
+        elif head_relation == Vector2(0,-1):
+            self.head = self.head_down
+
+    def update_tail_graphics(self):
+        tail_relation = self.body[-2] - self.body[-1]
+        if tail_relation == Vector2(1,0):
+            self.tail = self.tail_left
+        elif tail_relation == Vector2(-1,0):
+            self.tail = self.tail_right
+        elif tail_relation == Vector2(0,1):
+            self.tail = self.tail_up
+        elif tail_relation == Vector2(0,-1):
+            self.tail = self.tail_down
 
 
 
@@ -85,10 +170,10 @@ class Main:
 
     def check_fail(self):
         #check if snake is autside of the screen
-        if self.snake.body[0].x > cell_number or self.snake.body[0].x<0:
+        if self.snake.body[0].x > cell_number-1 or self.snake.body[0].x<0:
             self.game_over()
 
-        elif self.snake.body[0].y> cell_number or self.snake.body[0].y<0:
+        elif self.snake.body[0].y> cell_number-1 or self.snake.body[0].y<0:
             self.game_over()
 
         #check if snake does not colide with itself
